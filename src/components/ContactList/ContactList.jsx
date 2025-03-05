cb026b5b737bafc3e38d3a9dbca6fa23fadb98bb
CCBaiden <ceren.baiden@gmail.com>
2025-03-05 20:37:34 +0300

    added handledelete funct.

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,14 +1,24 @@
 import ContactItem from '../ContactItem/ContactItem'
 
-const ContactList = ({ contacts, filter }) => {
+const ContactList = ({ contacts, filter, setContacts }) => {
   const filteredContacts = contacts.filter((contact) =>
     contact.name.toLowerCase().includes(filter.toLowerCase())
   )
 
+  const handleDelete = (id) => {
+    setContacts((prevContacts) =>
+      prevContacts.filter((contact) => contact.id !== id)
+    )
+  }
+
   return (
     <ul>
       {filteredContacts.map((contact) => (
-        <ContactItem key={contact.id} contact={contact} />
+        <ContactItem
+          key={contact.id}
+          contact={contact}
+          onDelete={handleDelete} // onDelete fonksiyonunu ekliyoruz
+        />
       ))}
     </ul>
   )
