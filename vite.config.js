a3e54a97f259012e0710dcc1b89599ccea146180
CCBaiden <ceren.baiden@gmail.com>
2025-03-06 20:03:53 +0300

    .

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
+import { defineConfig } from 'vite'
+
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    port: 5173, // Sabit bir port numarası belirlemek için
   },
 })
